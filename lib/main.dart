4d17c8be0c5c4e7d27be8a447ce63b35c304ab72
Dieuvain Musaghi Carter <direction@gwinnerc.com>
2020-05-19 15:54:48 +0200

    Login Comments

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-  //Test Valid
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'LimangaMarket',
+
+      //Appel des Pages par Navigation
 
 routes: { '/login': (BuildContext context) =>loginPage() , '/register': (BuildContext context) =>RegisterPage()
 
@@ -47,15 +48,6 @@ textTheme: TextTheme(
 
         primaryColor: Colors.deepOrange[900],
         accentColor: Colors.amber[800],
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
